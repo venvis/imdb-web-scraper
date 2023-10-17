7025fe39274ec8eb1c7c7563b0186723f70c9d61
Vishal Venkat Raghavan <souljaboi943@gmail.com>
2023-10-17 17:58:22 +0200

    imdb-packages

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'An imdb search API that retreives the title , rank and year of necessary keywords.'
+LONG_DESCRIPTION = 'An imdb search API that retreives the title , rank and year of necessary keywords.'
+
+setup(
+    name="imdb-web-scraper",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Vishal",
+    author_email="vishalvenkat2604@gmail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='conversion',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
